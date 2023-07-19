617110bc7b2e5eb892949de4643a64138b93fc6c
EnrickyHip <enricky.hipolito@gmail.com>
2023-07-19 14:25:05 -0300

    organize reducer

--- src/Context/reducer.ts
+++ src/Context/reducer.ts
@@ -18,16 +18,16 @@ export const initialState: ThemeState = {
   mode: ThemeMode.LIGHT,
 };
 
-export const reducer = (state: ThemeState = { ...initialState }, action: ThemeAction) => {
+export const reducer = (state: ThemeState = { ...initialState }, action: ThemeAction): ThemeState => {
   switch (action.type) {
+    case ThemeTypeAction.SET_THEME:
+      return { ...state, mode: action.payload ? action.payload : ThemeMode.LIGHT };
+
     case ThemeTypeAction.TOGGLE_THEME: {
       const theme = state.mode === ThemeMode.DARK ? ThemeMode.LIGHT : ThemeMode.DARK;
       saveTheme(theme);
       return { ...state, mode: theme };
     }
-
-    case ThemeTypeAction.SET_THEME:
-      return { ...state, mode: action.payload ? action.payload : ThemeMode.LIGHT };
 
     default:
       return { ...state };
